eda3162c54005c91bd80e37593780f3e5f0d83ae
Spin0382 <kenliddz@gmail.com>
2026-04-14 02:54:15 -0400

    Optimizaciones: Room indices, ViewModel debounce, ProductoCard sin corrutinas, HTTP cache

--- app/src/main/java/com/cecosesola/coop/data/local/PreciosDatabase.kt
+++ app/src/main/java/com/cecosesola/coop/data/local/PreciosDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [ProductoEntity::class, MetadataEntity::class, FavoritoEntity::class, BusquedaEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class PreciosDatabase : RoomDatabase() {
